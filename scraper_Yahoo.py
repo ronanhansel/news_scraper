02dc6312ab6d45fb51b4e46e5bbdd2587325b4ba
Trần Vũ Mạnh Đức <ronanhansel@gmail.com>
2023-10-20 14:52:57 +0700

    yahoo

--- scraper_Yahoo.py
+++ scraper_Yahoo.py
@@ -28,35 +28,43 @@ with open('Yahoo_output_all.csv', 'w', newline='') as file:
     writer.writerow(['Author', 'Title', 'Content'])
     driver.get('https://finance.yahoo.com/topic/economic-news')
 
-    # i = 0
-    # SLEEP_TIME = 1;
-
-    # while i < 3:
-    #     html = driver.find_element(By.TAG_NAME, 'html')
-    #     html.send_keys(Keys.END)
-    #     time.sleep(SLEEP_TIME)
-    #     i += 1
-    #     print(f'Scrolling {i}')
+    i = 0
+    SLEEP_TIME = 1;
+    SCROLL_TIME = 15;
+    while i < SCROLL_TIME:
+        html = driver.find_element(By.TAG_NAME, 'html')
+        html.send_keys(Keys.END)
+        time.sleep(SLEEP_TIME)
+        i += 1
+        print(f'Scrolling {i}')
     elems = driver.find_elements(By.XPATH, "//a[@href]")
     links = []
-    for elem in elems:
-      link = [a for a in elem.get_attribute("href").split('/') if a != '']
+    for no in range(len(elems)):
+      elem = elems[no]
+      try:
+        link = [a for a in elem.get_attribute("href").split('/') if a != '']
+      except StaleElementReferenceException:
+        print('Stale Element. Retrying...')
+        elems = driver.find_elements(By.XPATH, "//a[@href]")
+        elem = elems[no]
+        link = [a for a in elem.get_attribute("href").split('/') if a != '']
       if len(link) > 3:
         if link[1] in ['finance.yahoo.com'] and link[2] in ['news', 'm']:
           link[0] = link[0] + '/'
           links.append('/'.join(link))
     
-    driver.get(links[0])
-    read_more = driver.find_element(By.TAG_NAME, 'button').\
-                                    find_element(By.CLASS_NAME, 'collapse-button')
-    read_more.click()
-    c = Content(
-      author=driver.find_element(By.CLASS_NAME, 'caas-author-byline-collapse').text,
-      title=driver.find_element(By.CLASS_NAME, 'caas-title-wrapper').text,
-      article='\n'.join([a.text for a in driver.\
-                                   find_element(By.CLASS_NAME, "caas-body").\
-                                   find_elements(By.TAG_NAME, 'p')])
-    )
-    print(c.author, c.title, c.article)
+    print(f'{len(links)} available.')
     
+    for l in links:
+      driver.get(l)
+      c = Content(
+        author=driver.find_element(By.CLASS_NAME, 'caas-author-byline-collapse').text,
+        title=driver.find_element(By.CLASS_NAME, 'caas-title-wrapper').text,
+        article=''.join([a for a in driver.\
+                                    find_element(By.CLASS_NAME, "caas-body").\
+                                    get_attribute('textContent')])
+      )
+      print(c.author, c.title, c.article[:50])
+      writer.writerow([c.author, c.title, c.article])
+      
     
